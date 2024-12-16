8e3d77405e6613e5b46abdaf720b8ce62921da13
Prughvi <prudhviraj3525@gmail.com>
2024-12-16 16:38:15 +0530

    user

--- run.sh
+++ run.sh
@@ -1,7 +1,7 @@
 git pull ;
 if [ $#-ne 2 ]; then
-  echo Input Missing
-  echo Usage:run.sh <coponent-name> <env>
+  echo "Input Missing"
+  echo "Usage:run.sh <coponent-name> <env>"
   exit 1
 fi
 ansible-playbook -i $1-$2.devops11.online, -e ansible_user=ec2-user -e ansible_password=DevOps321 -e env=dev -e App_name=$1 roboshop.yml
